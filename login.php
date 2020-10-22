5fe610cf6b8db79d3f0e9fb16f39b319aa72ce37
PhucDuong-SSS <phucdnDev@gmail.com>
2020-10-22 23:01:03 +0700

    v1.0

---
+++ login.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Student Attendance System in PHP using Ajax</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="jumbotron text-center" style="margin-bottom:0">
+  <h1>Student Attendance System</h1>
+</div>
+
+
+<div class="container">
+  <div class="row">
+    <div class="col-md-4">
+
+    </div>
+    <div class="col-md-4" style="margin-top:20px;">
+      <div class="card">
+        <div class="card-header">Teacher Login</div>
+        <div class="card-body">
+          <form method="post" id="teacher_login_form">
+            <div class="form-group">
+              <label>Enter Email Address</label>
+              <input type="text" name="teacher_emailid" id="teacher_emailid" class="form-control" />
+              <span id="error_teacher_emailid" class="text-danger"></span>
+            </div>
+            <div class="form-group">
+              <label>Enter Password</label>
+              <input type="password" name="teacher_password" id="teacher_password" class="form-control" />
+              <span id="error_teacher_password" class="text-danger"></span>
+            </div>
+            <div class="form-group">
+              <input type="submit" name="teacher_login" id="teacher_login" class="btn btn-info" value="Login" />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4">
+
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
